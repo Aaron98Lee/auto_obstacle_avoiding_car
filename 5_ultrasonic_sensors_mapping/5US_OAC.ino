41139cbc1ef2e4eb2c0195047a972bfbc8610f7c
Aaron98Lee <62461282+Aaron98Lee@users.noreply.github.com>
2020-08-22 00:27:49 +0800

    Update 5US_OAC.ino
    Latest update with correct coding

--- 5_ultrasonic_sensors_mapping/5US_OAC.ino
+++ 5_ultrasonic_sensors_mapping/5US_OAC.ino
@@ -1,227 +1,168 @@
-#include <Servo.h>          //standard library for the servo
-#include <NewPing.h>        //for the Ultrasonic sensor function library.const int frontEchoPin = 7;
-
-//L298N motor control pins
-const int ENA = 5;
-const int RightMotorForward = 4;
-const int RightMotorBackward = 3;
-const int LeftMotorForward = 9;
-const int LefttMotorBackward = 10;
-const int ENB = 11;
-
-//sensor pins
-#define trig_front A0 //analog input 0
-#define echo_front A1 //analog input 1
-#define trig_L1 A2 //analog input 2
-#define echo_L1 A3 //analog input 3
-#define trig_R1 A4 //analog input 4
-#define echo_R1 A5 //analog input 5
-#define trig_L2 A6 //analog input 6
-#define echo_L2 A7 //analog input 7
-#define trig_R2 A8 //analog input 8
-#define echo_R2 A9 //analog input 9
-
-#define maximum_distance 200
-boolean goesForward = false;
-int distance = 100;
-
-NewPing sonar(trig_pin, echo_pin, maximum_distance); //sensor function
-Servo servo_motor;
-
-void setup() {
-  // serial
-  Serial.begin(9600);
-  
-  // ultrasonic
-  pinMode(trig_front, OUTPUT);
-  pinMode(echo_front, INPUT);
-  pinMode(trig_L1, OUTPUT);
-  pinMode(echo_L1, INPUT);
-  pinMode(trig_R1, OUTPUT);
-  pinMode(echo_R1, INPUT);
-  pinMode(trig_L2, OUTPUT);
-  pinMode(echo_L2, INPUT);
-  pinMode(trig_R2, OUTPUT);
-  pinMode(echo_R2, INPUT);
-  analogWrite(ENA, 80);
-  analogWrite(ENB, 88);
-  
-  // motors
-  pinMode(LeftMotorForward, OUTPUT);
-  pinMode(LeftMotorBackward, OUTPUT);
-  pinMode(ENA, OUTPUT)
-  pinMode(RightMotorForward, OUTPUT);
-  pinMode(RightMotorBackward, OUTPUT);
-  pinMode(ENB, OUTPUT);
+/*  OBSTACLE AVOIDANCE ROBOT */
+int echoPinRight = A1;         // PIN A1 TO RECEIVE ECHO FROM RIGHT SENSOR
+int trigerPinRight = A2;       // PIN A2 TO TRIGER THE RIGHT SENSOR
+int echoPinMiddle = A3;        // PIN A3 TO RECEIVE ECHO FROM MIDDLE SENSOR
+int trigerPinMiddle = A4;      // PIN A4 TO TRIGER THE MIDDLE SENSOR
+int echoPinLeft = A5;         // PIN A5 TO RECEIVE ECHO FROM LEFT SENSOR
+int trigerPinLeft = A6;       // PIN A6 TO TRIGER THE LEFT SENSOR
+int echoPinSideRight = A7;        // PIN A7 TO RECEIVE ECHO FROM SIDE RIGHT SENSOR
+int trigerPinSideRight = A8;      // PIN A8 TO TRIGER THE SIDE RIGHT SENSOR
+int echoPinSideLeft = A9;         // PIN A9 TO RECEIVE ECHO FROM SIDE LEFT SENSOR
+int trigerPinSideLeft = A10;       // PIN A10 TO TRIGER THE SIDE LEFT SENSOR
+int motorRF = 2;              // PIN 2 FOR CONTROLLING MOTOR A1
+int motorRB = 3;              // PIN 3 FOR CONTROLLING MOTOR A2
+int motorLF = 4;              // PIN 4 FOR CONTROLLING MOTOR B1
+int motorLB = 5;              // PIN 4 FOR CONTROLLING MOTOR B1
+int motorSpeed = 11;          // ARDUINO PWM PIN 11 TO CONTROL MOTORS SPEED
+double echoValueright, echoValuemiddle, echoValueleft, echoValueSideLeft, echoValueSideRight;  
+// INITIALIZING VARIABLE TO RECEIVE ECHO FROM THE SENSORS
+float cmRight, cmMiddle, cmLeft, cmSLeft, cmSRight;                           
+ // INITIALIZING VARIABLES TO STORE DISTANCE MEASURED OF SENSORS IN CENTIMETERS
+void setup()
+{
+  Serial.begin(9600);                                     // STARTING SERIAL COMMUNICATION TO PRINT VALUES RECEIVED BY SENSOR ON SERIAL MONITER
+  pinMode(echoPinLeft, INPUT);                           // SETTING ARDUINO PIN AS INPUT
+  pinMode(trigerPinLeft, OUTPUT);                       // SETTING ARDUINO PIN AS OUTPUT
+  pinMode(echoPinMiddle, INPUT);                        // SETTING ARDUINO PIN AS INPUT
+  pinMode(trigerPinMiddle, OUTPUT);                    // SETTING ARDUINO PIN AS OUTPU
+  pinMode(echoPinRight, INPUT);                        // SETTING ARDUINO PIN AS INPUT
+  pinMode(trigerPinRight, OUTPUT);                     // SETTING ARDUINO PIN AS OUTPU
+  pinMode(echoPinSideLeft, INPUT);                        // SETTING ARDUINO PIN AS INPUT
+  pinMode(trigerPinSideLeft, OUTPUT);                     // SETTING ARDUINO PIN AS OUTPU
+  pinMode(echoPinSideRight, INPUT);                        // SETTING ARDUINO PIN AS INPUT
+  pinMode(trigerPinSideRight, OUTPUT);                     // SETTING ARDUINO PIN AS OUTPU
+  pinMode(motorSpeed, OUTPUT);          // SETTING ARDUINO PIN AS OUTPUT TO CONTROL MOTOR SPEED
 }
+void loop()
+{
+  float cmR, cmM, cmL, cmSL, cmSR;                                      // INITIALIZING VARIABLES TO STORE DISTANCE OBTAINED FROM SENSORS IN CM
+  int maxRange = 30;                                        // SETTING MAX RANGE 
+  analogWrite(motorSpeed, 90);                              // SETTING MOTORS SPEED YOU CAN SET SPEED BETWEEN 0-255
+  digitalWrite(motorRF, HIGH);
+  digitalWrite(motorRB, LOW);
+  digitalWrite(motorLF, HIGH);
+  digitalWrite(motorLB, LOW);
 
-void loop() {
-  Serial.begin(9600);
-  
-  long duration_front;
-  long distance_front;
-  long duration_L1;
-  long distance_L1;
-  long duration_R1;
-  long distance_R1;
-  long duration_L2;
-  long distance_L2;
-  long duration_R2;
-  long distance_R2;
-
-
-//Calculating distance
-
-digitalWrite(trig_front, LOW);
-delay(2);
-digitalWrite(trig_front, HIGH);
-delay(10);
-digitalWrite(trig_front, LOW);
-duration_front = pulseIN(echo_front, HIGH);
-distance_front = duration_front*0.034/2;
-
-digitalWrite(trig_L1, LOW);
-delay(2);
-digitalWrite(trig_L1, HIGH);
-delay(10);
-digitalWrite(trig_L1, LOW);
-duration_left = pulseIN(echo_L1, HIGH);
-distance_left = duration_front*0.034/2;
-
-digitalWrite(trig_R1, LOW);
-delay(2);
-digitalWrite(trig_R1, HIGH);
-delay(10);
-digitalWrite(trig_R1, LOW);
-duration_right = pulseIN(echo_R1, HIGH);
-distance_right = duration_front*0.034/2;
-
-digitalWrite(trig_L2, LOW);
-delay(2);
-digitalWrite(trig_L2, HIGH);
-delay(10);
-digitalWrite(trig_L2, LOW);
-duration_right = pulseIN(echo_L2, HIGH);
-distance_right = duration_front*0.034/2;
-
-digitalWrite(trig_R2, LOW);
-delay(2);
-digitalWrite(trig_R2, HIGH);
-delay(10);
-digitalWrite(trig_R2, LOW);
-duration_right = pulseIN(echo_R2, HIGH);
-distance_right = duration_front*0.034/2;
-  
-
-if (distance_front <=  20){
-
-    moveStop();
-    delay(30);
-    moveForward();
-    
-    if(distance_L1 > 10 && distance_L1 < 20 && distance_L2 <20){
-      moveForward();
-    }
-    if(distance_L1 && distance_L2>= 20){
-       turnLeft();
-       delay(30);
-       moveForward();
-    }
-    if(distance_L1 < 10 && distance_L1 > 0 && distance_R2 > 0){
-      turnRight;
-      delay(30);
-      moveForward();
-    }
- } 
-  
-  if(distance_front <= 20 && distance_R1 > 20 && distance_R2 >20){
-    moveStop();
-    delay(1000);
-    moveStop();
-    delay(400);
-    
-  }
-
-  if(distance_front <= 20 && distance_R1 < 20 && distance_R2 <20){
-    moveStop();
-    delay(1000);
-    turnRight;
-    delay(800);
    
+  cmR = sensorRight();
+  cmM = sensorMiddle();
+  cmL = sensorLeft();
+  cmSL = sensorSideLeft();
+  cmSR = sensorSideRight();
+  Serial.println();
+  Serial.print(cmL);
+  Serial.print("        ");
+  Serial.print(cmM);
+  Serial.print("        ");
+  Serial.print(cmR);
+  Serial.print("        ");
+  Serial.print(cmSL);
+  Serial.print("        ");
+  Serial.print(cmSR);
+  if (cmR && cmSR < maxRange)
+  {
+    digitalWrite(motorRF, LOW);
+    digitalWrite(motorRB, HIGH);
+    digitalWrite(motorLF, HIGH);
+    digitalWrite(motorLB, LOW);
   }
-  
-  
-}
-
-
-void moveStop(){
-  
-  digitalWrite(RightMotorForward, LOW);
-  digitalWrite(LeftMotorForward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
-  digitalWrite(LeftMotorBackward, LOW);
-}
-
-void moveForward(){
-
-  if(!goesForward){
-
-    goesForward=true;
-    
-    digitalWrite(LeftMotorForward, HIGH);
-    digitalWrite(RightMotorForward, HIGH);
-  
-    digitalWrite(LeftMotorBackward, LOW);
-    digitalWrite(RightMotorBackward, LOW); 
+  if (cmL && cmSL< maxRange)
+  {
+    digitalWrite(motorRF, HIGH);
+    digitalWrite(motorRB, LOW);
+    digitalWrite(motorLF, LOW);
+    digitalWrite(motorLB, HIGH);
   }
-}
 
-void moveBackward(){
-
-  goesForward=false;
-
-  digitalWrite(LeftMotorBackward, HIGH);
-  digitalWrite(RightMotorBackward, HIGH);
-  
-  digitalWrite(LeftMotorForward, LOW);
-  digitalWrite(RightMotorForward, LOW);
-  
-}
-
-void turnRight(){
-
-  digitalWrite(LeftMotorForward, HIGH);
-  digitalWrite(RightMotorBackward, HIGH);
-  
-  digitalWrite(LeftMotorBackward, LOW);
-  digitalWrite(RightMotorForward, LOW);
-  
-  delay(500);
-  
-  digitalWrite(LeftMotorForward, HIGH);
-  digitalWrite(RightMotorForward, HIGH);
-  
-  digitalWrite(LeftMotorBackward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
- 
-  
-  
-}
-
-void turnLeft(){
-
-  digitalWrite(LeftMotorBackward, HIGH);
-  digitalWrite(RightMotorForward, HIGH);
-  
-  digitalWrite(LeftMotorForward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
+  if (cmM < maxRange) { if (cmL || cmSL > cmR && cmSR)
+    {
+      digitalWrite(motorRF, HIGH);
+      digitalWrite(motorRB, LOW);
+      digitalWrite(motorLF, LOW);
+      digitalWrite(motorLB, HIGH);
+    }
+    else if (cmR || cmSR > cmL && cmSL)
+    {
+      digitalWrite(motorRF, LOW);
+      digitalWrite(motorRB, HIGH);
+      digitalWrite(motorLF, HIGH);
+      digitalWrite(motorLB, LOW);
+    }
+    else if (cmR && cmSR < maxRange && (cmL && cmSL) < maxRange)
+    {
+      digitalWrite(motorRF, LOW);
+      digitalWrite(motorRB, HIGH);
+      digitalWrite(motorLF, LOW);
+      digitalWrite(motorLB, HIGH);
+    }
+  }
 
   delay(500);
-  
-  digitalWrite(LeftMotorForward, HIGH);
-  digitalWrite(RightMotorForward, HIGH);
-  
-  digitalWrite(LeftMotorBackward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
+}
+float sensorRight()               // FUUNCTION TO OPERATE RIGHT SENSOR
+{
+  digitalWrite(trigerPinRight, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigerPinRight, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigerPinRight, LOW);
+  echoValueright = pulseIn(echoPinRight, HIGH);
+  cmRight = echoValueright / 59;
+  //inchesRight=cmRight/2.54;
+  //Serial.print(cmRight);
+  return (cmRight);
+}
+float sensorSideRight()               // FUUNCTION TO OPERATE RIGHT SENSOR
+{
+  digitalWrite(trigerPinSideRight, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigerPinSideRight, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigerPinSideRight, LOW);
+  echoValueright = pulseIn(echoPinSideRight, HIGH);
+  cmSRight = echoValueSideRight / 59;
+  //inchesRight=cmRight/2.54;
+  //Serial.print(cmRight);
+  return (cmRight);
+}
+float sensorMiddle()              // FUNCTION TO OPERATE MIDDLE SENSOR
+{
+  digitalWrite(trigerPinMiddle, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigerPinMiddle, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigerPinMiddle, LOW);
+  echoValuemiddle = pulseIn(echoPinMiddle, HIGH);
+  cmMiddle = echoValuemiddle / 59;
+
+  //inchesMiddle=cmMiddle/2.54;
+  //Serial.print(cmMiddle);
+  return (cmMiddle);
+}
+float sensorLeft()              // FUNCTION TO OPERATE LETT SENSOR
+{
+  digitalWrite(trigerPinLeft, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigerPinLeft, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigerPinLeft, LOW);
+  echoValueleft = pulseIn(echoPinLeft, HIGH);
+  cmLeft = echoValueleft / 59;
+  //inchesLeft=cmLeft/2.54;
+  //Serial.print(cmLeft);
+  return (cmLeft);
 }
 
+float sensorSideLeft()              // FUNCTION TO OPERATE LETT SENSOR
+{
+  digitalWrite(trigerPinSideLeft, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigerPinSideLeft, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigerPinSideLeft, LOW);
+  echoValueleft = pulseIn(echoPinSideLeft, HIGH);
+  cmSLeft = echoValueSideLeft / 59;
+  //inchesLeft=cmLeft/2.54;
+  //Serial.print(cmLeft);
+  return (cmLeft);
+}
